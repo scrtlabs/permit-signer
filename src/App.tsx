2c23aaa3f37cb8108aaf702d7665d6c8eb252629
Cashmaney <itzik@enigma.co>
2022-04-10 18:13:27 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Permit Signer
       </header>
+      <form style={{width: "50%"}}>
+          <div className="form-group">
+            <label htmlFor="permitNameInput" style={{display: "flex", alignItems: "left"}}>Permit Name</label>
+            <input className="form-control" id="permitNameInput" aria-describedby="permitName" placeholder="permit123" />
+          </div>
+          <div className="form-group">
+              <label htmlFor="addressInput"  style={{display: "flex", alignItems: "left"}}>Address</label>
+              <input className="form-control" id="addressInput" aria-describedby="address" placeholder="secret1..." />
+          </div>
+              <label htmlFor="permissionsInput"  style={{display: "flex", alignItems: "left"}}>Permissions</label>
+              <input className="form-control" id="permissionsInput" aria-describedby="permissions" placeholder="owner, balance, ..." />
+          <div className="form-group">
+          </div>
+          <div className="form-group">
+              <label htmlFor="memoInput"  style={{display: "flex", alignItems: "left"}}>Memo</label>
+              <input className="form-control" id="memoInput" aria-describedby="memo" placeholder="" />
+          </div>
+          <div className="form-group">
+              <label htmlFor="chainIdInput"  style={{display: "flex", alignItems: "left"}}>Chain ID</label>
+              <input className="form-control" id="chainIdInput" aria-describedby="chainId" placeholder="secret-4" />
+          </div>
+          <button type="submit" className="btn btn-primary">Sign</button>
+      </form>
     </div>
   );
 }
